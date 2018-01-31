6c7ff559b6040026a3fee7b487a5da0c1fddf4d8
suclearnub <suclearnub00@gmail.com>
2018-01-31 16:30:02 +0800

    Perform code cleanup

--- src/main/java/frc/team7242/robot/Robot.java
+++ src/main/java/frc/team7242/robot/Robot.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package org.usfirst.frc.team7242.robot;
+package frc.team7242.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
@@ -23,92 +23,89 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends IterativeRobot {
-	private static final String kDefaultAuto = "Default";
-	private static final String kCustomAuto = "My Auto";
-	private String m_autoSelected;
-	private SendableChooser<String> m_chooser = new SendableChooser<>();
-	
-	Joystick driverStick = new Joystick(0);
-	XboxController xboxdriver = new XboxController(1); //xbox controller in port 1
-	
-	Spark leftFront = new Spark(0);
-	Spark rightFront = new Spark(1);
-	Spark leftBack = new Spark(2);
-	Spark rightBack = new Spark(3);
-	
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
-	@Override
-	public void robotInit() {
-		m_chooser.addDefault("Default Auto", kDefaultAuto);
-		m_chooser.addObject("My Auto", kCustomAuto);
-		SmartDashboard.putData("Auto choices", m_chooser);
-	}
+    private static final String kDefaultAuto = "Default";
+    private static final String kCustomAuto = "My Auto";
+    private String m_autoSelected;
+    private SendableChooser<String> m_chooser = new SendableChooser<>();
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * <p>You can add additional auto modes by adding additional comparisons to
-	 * the switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
-	@Override
-	public void autonomousInit() {
-		m_autoSelected = m_chooser.getSelected();
-		// autoSelected = SmartDashboard.getString("Auto Selector",
-		// defaultAuto);
-		System.out.println("Auto selected: " + m_autoSelected);
-	}
+    Joystick driverStick = new Joystick(0);
+    XboxController xboxdriver = new XboxController(1); //xbox controller in port 1
 
-	/**
-	 * This function is called periodically during autonomous.
-	 */
-	@Override
-	public void autonomousPeriodic() {
-		switch (m_autoSelected) {
-			case kCustomAuto:
-				// Put custom auto code here
-				break;
-			case kDefaultAuto:
-			default:
-				// Put default auto code here
-				break;
-		}
-	}
+    Spark leftFront = new Spark(0);
+    Spark rightFront = new Spark(1);
+    Spark leftBack = new Spark(2);
+    Spark rightBack = new Spark(3);
 
-	/**
-	 * This function is called periodically during operator control.
-	 */
-	@Override
-	public void teleopPeriodic() {
-		
-		//if using joystick
-		double leftStickValue = driverStick.getRawAxis(1);
-		double rightStickValue = driverStick.getRawAxis(3);
-		
-		//if using xbox, idk how to map xbox
-	//	double leftStickvalue = xboxdriver.getX();
-	//	double rightStickvalue = xboxdriver.getY();
-		
-		
-		
-		// runs motors at speed
-		leftFront.set(leftStickValue);
-		leftBack.set(leftStickValue);
-		rightFront.set(rightStickValue);
-		rightBack.set(rightStickValue);
-	}
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    @Override
+    public void robotInit() {
+        m_chooser.addDefault("Default Auto", kDefaultAuto);
+        m_chooser.addObject("My Auto", kCustomAuto);
+        SmartDashboard.putData("Auto choices", m_chooser);
+    }
 
-	/**
-	 * This function is called periodically during test mode.
-	 */
-	@Override
-	public void testPeriodic() {
-	}
+    /**
+     * This autonomous (along with the chooser code above) shows how to select
+     * between different autonomous modes using the dashboard. The sendable
+     * chooser code works with the Java SmartDashboard. If you prefer the
+     * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+     * getString line to get the auto name from the text box below the Gyro
+     *
+     * <p>You can add additional auto modes by adding additional comparisons to
+     * the switch structure below with additional strings. If using the
+     * SendableChooser make sure to add them to the chooser code above as well.
+     */
+    @Override
+    public void autonomousInit() {
+        m_autoSelected = m_chooser.getSelected();
+        // autoSelected = SmartDashboard.getString("Auto Selector",
+        // defaultAuto);
+        System.out.println("Auto selected: " + m_autoSelected);
+    }
+
+    /**
+     * This function is called periodically during autonomous.
+     */
+    @Override
+    public void autonomousPeriodic() {
+        switch (m_autoSelected) {
+            case kCustomAuto:
+                // Put custom auto code here
+                break;
+            case kDefaultAuto:
+            default:
+                // Put default auto code here
+                break;
+        }
+    }
+
+    /**
+     * This function is called periodically during operator control.
+     */
+    @Override
+    public void teleopPeriodic() {
+
+        //if using joystick
+        double leftStickValue = driverStick.getRawAxis(1);
+        double rightStickValue = driverStick.getRawAxis(3);
+        //if using xbox, idk how to map xbox
+        //	double leftStickvalue = xboxdriver.getX();
+        //	double rightStickvalue = xboxdriver.getY();
+
+        // runs motors at speed
+        leftFront.set(leftStickValue);
+        leftBack.set(leftStickValue);
+        rightFront.set(rightStickValue);
+        rightBack.set(rightStickValue);
+    }
+
+    /**
+     * This function is called periodically during test mode.
+     */
+    @Override
+    public void testPeriodic() {
+    }
 }
