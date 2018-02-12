519e70334b78d73f58edba01e2c5de1fb953492d
Unknown <victorfong77@gmail.com>
2018-02-12 18:22:48 +0800

    for oz

--- src/main/java/frc/team7242/robot/Robot.java
+++ src/main/java/frc/team7242/robot/Robot.java
@@ -24,12 +24,34 @@ public class Robot extends IterativeRobot {
     private SendableChooser<String> m_chooser = new SendableChooser<>();
     Joystick driverStick = new Joystick(0);
 
-    double autonomousTime = 5;
+
+
+   // double autonomousTime = 3;
+    //double autonomousTime2 = 4;
+   // double autonomousTime3 = 5.5;
+   // double autonomousTime4 = 6;
+
+    double autonomousTime5 = 1.5;
+    double autonomousTime6 = 3.5;
+    double autonomousTime7 = 4.5;
+    double autonomousTime8 = 6;
+    double autonomousTime9 = 9;
+    double autonomousTime10= 10;
+    double autonomousTime11= 11.5;
+    double autonomousTime12= 12;
+
+
+    public Spark leftFront = new Spark(0);
+    public Spark rightFront = new Spark(2);
+    public Spark leftBack = new Spark(1);
+    public Spark rightBack = new Spark(3);
 
     double autonomousStartTime;
 
-    ScalableSpeedControllerGroup right = new ScalableSpeedControllerGroup(0.75, new Spark(0), new Spark(1));
-    ScalableSpeedControllerGroup left = new ScalableSpeedControllerGroup(1.0, new Spark(2), new Spark(3));
+    ScalableSpeedControllerGroup right = new ScalableSpeedControllerGroup(0.75, leftFront, leftBack);
+    ScalableSpeedControllerGroup left = new ScalableSpeedControllerGroup(0.72, rightFront, rightBack);
+
+
 
     DifferentialDrive drive = new DifferentialDrive(left, right);
 
@@ -42,7 +64,10 @@ public class Robot extends IterativeRobot {
         left.setInverted(true);
         right.setInverted(true);
 
-
+       // leftFront.setSafetyEnabled(false);
+       // rightFront.setSafetyEnabled(false);
+       // leftBack.setSafetyEnabled(false);
+       // rightBack.setSafetyEnabled(false);
     }
 
     public void autonomousInit() {
@@ -55,14 +80,39 @@ public class Robot extends IterativeRobot {
 
     public void autonomousPeriodic() {
         double deltaTime = Timer.getFPGATimestamp() - autonomousStartTime;
-        if (deltaTime < autonomousTime) {
-            drive.arcadeDrive(0.5, 0.0);
-        } else {
-            drive.arcadeDrive(0.0, 0.0);
+       // if (deltaTime < autonomousTime) {
+          //  drive.arcadeDrive(0.80, 0.0);
+        //} else if (autonomousTime < deltaTime && deltaTime < autonomousTime2) {
+            //drive.arcadeDrive(0.0, 0.0);
+       // } else if (autonomousTime2 < deltaTime && deltaTime < autonomousTime3) {
+        //    drive.arcadeDrive(0.0, -0.56);
+       // } else if (autonomousTime3 < deltaTime && deltaTime < autonomousTime4) {
+       //     drive.arcadeDrive(1, 0.0);
+       // } else {
+       //     drive.arcadeDrive(0.0, 0.0);
+       // }
+
+   // }
+    if(deltaTime < autonomousTime5){
+        drive.arcadeDrive(0.0, 0.56);
+    }else if(autonomousTime5 < deltaTime && deltaTime < autonomousTime6){
+        drive.arcadeDrive(0.8, 0.0);
+    }else if(autonomousTime6 < deltaTime && deltaTime < autonomousTime7){
+        drive.arcadeDrive(0.0, 0.0);
+    }else if(autonomousTime7 < deltaTime && deltaTime < autonomousTime8){
+        drive.arcadeDrive(0.0, -0.65);
+    }else if(autonomousTime8 < deltaTime && deltaTime < autonomousTime9) {
+        drive.arcadeDrive(0.8, 0.0);
+     }else if(autonomousTime9 < deltaTime && deltaTime < autonomousTime10){
+        drive.arcadeDrive(0.0, 0.0);
+    }else if(autonomousTime10 < deltaTime && deltaTime < autonomousTime11){
+        drive.arcadeDrive(0.0, -0.56);
+    }else if(autonomousTime11 < deltaTime && deltaTime < autonomousTime12){
+        drive.arcadeDrive(1, 0.0);
+    }else{
+        drive.arcadeDrive(0.0, 0.0);
+}
         }
-
-
-    }
 
     public void teleopPeriodic() {
 
@@ -86,11 +136,18 @@ public class Robot extends IterativeRobot {
             braking = 1;
         }
 
-            double sens = 2.5;
+        double sens = 2;
+        double threshold = 0.1;
+        double tres;
+        if((Math.abs(xvalue)<threshold) &&  (Math.abs(yvalue)<threshold)){
+            tres = 0;
+        }
+        else {
+            tres = 1;
+        }
 
-        right.set((yvalue + xvalue/sens) * (0.5) * boost * braking * 1.1);
-        left.set((yvalue - xvalue/sens) * (-0.5) * boost);
+
+        right.set((yvalue - xvalue/sens) * (0.5) * boost * braking * 1 * tres);
+        left.set((yvalue + xvalue/sens) * (-0.5) * boost * braking * tres);
     }
 }
-
-
